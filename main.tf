99e558c60e961ce30269aba70926eaed0ea4ebe4
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-07-11 08:11:51 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,14 @@ terraform {
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix =  "sankari"
 }
+
+terraform {
+  required_version = ">= 1.6.0, < 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
