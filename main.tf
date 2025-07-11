89fbe24547748157d2c10842cbf6b3969562e8de
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-07-11 21:21:26 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "sctp-ce10-tfstate" # Change this if needed
+    key    = "env/dev/terraform.tfstate" # Change this if needed
     region = "ap-southeast-1"
   }
 }
@@ -14,24 +14,6 @@ resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "sankari"
 }
 
-resource "aws_s3_bucket_versioning" "s3_tf_versioning" {
-  bucket = aws_s3_bucket.s3_tf.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket" "logging_bucket" {
-  bucket = "s3-logging-bucket-example"
-}
-
-resource "aws_s3_bucket_logging" "log" {
-  bucket = aws_s3_bucket.s3_tf.id
-
-  target_bucket = aws_s3_bucket.logging_bucket.id
-  target_prefix = "log/"
-}
 
 terraform {
   required_version = ">= 1.6.0"
